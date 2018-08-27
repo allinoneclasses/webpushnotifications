9604b0d43e3020bbf2ed36b26d787f5086d2feb1
AashishSharma <ashishrsharma2@gmail.com>
2018-08-27 20:27:46 +0530

    test node app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
    res.send('Hello World');
