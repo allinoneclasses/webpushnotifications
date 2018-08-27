bc11d10fe9352784b8bbdb647d6bb1200d3a2f42
AashishSharma <ashishrsharma2@gmail.com>
2018-08-27 20:17:42 +0530

    test node app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+ var app = express();
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/',function(req,res){
@@ -9,6 +9,6 @@ express()
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-express.get('/ashish', function(req,res){
+app.get('/ashish', function(req,res){
 	res.sendFile('abc.html');
 })
