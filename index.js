4f497f7f2da7466b11841d4387f53a5320357fd9
AashishSharma <ashishrsharma2@gmail.com>
2018-08-27 20:35:10 +0530

    test node app

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-app.listen(listen(PORT, () => console.log(`Listening on ${ PORT }`)));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+ var app = new express();
+app.use(express.static(path.join(__dirname, 'public')))
+  .get('/',function(req,res){
+     res.sendFile('index.html');
+})
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/ashish', function(req,res){
+	console.log('ashish');
+	res.sendFile('abc.html');
+	console.log('sharma');
+})
