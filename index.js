24665b78535fa0cce2c7c400acd95d9f3ef20d53
AashishSharma <ashishrsharma2@gmail.com>
2018-08-27 21:03:54 +0530

    test node app

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const path = require('path');
 const PORT = process.env.PORT || 5000
 app.get('/ashish', function (req, res) {
    res.send('Hello World');
+   if (!('serviceWorker' in navigator)) {
+  console.log('Push isnt supported on this browser, disable or hide UI.');
+  return;
+}
+if (!('PushManager' in window)) {
+  console.log('Push isnt supported on this browser, disable or hide UI.');
+  return;
+}
+console.log('SERVICE WORKER SUPPORTED');
 })
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req,res){
 	console.log(__dirname);
-	res.sendFile('index.html');
+	res.sendFile('index.js');
 	console.log('sharma');
 })
 
