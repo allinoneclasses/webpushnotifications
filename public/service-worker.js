52d8bb6a2ff648baf0d0df401317491767757b61
pujac70 <chauhan.puja777@gmail.com>
2018-08-31 10:57:11 +0530

    Changes for sending custom notifications.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,12 +4,27 @@ self.addEventListener('push', function(event) {
   } else {
     console.log('This push event has no data.');
   }
-  const title = 'Simple Title';
+  const data = event.data.json();
+  console.log('Title ', data.header);
+  console.log('body ', data.message);
+  const title = data.header;
     const options = {
-      body: event.data.text()
+      body: data.message,
+      badge: '/principal-logo.png',
+      icon: '/principal-logo.png',
+      requireInteraction: true
     };
 	const promiseChain = self.registration.showNotification(title,options);
 
   event.waitUntil(promiseChain);
  
+});
+
+self.addEventListener('notificationclick', function(event) {
+  const clickedNotification = event.notification;
+  clickedNotification.close();
+  
+  // Do something as the result of the notification click
+  const promiseChain = clients.openWindow('https://www.google.com');
+  event.waitUntil(promiseChain);
 });
